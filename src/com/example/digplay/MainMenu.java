bae421222a395f2e8e461805e4d7d8f628334c3f
zacharynanfelt <zacharynanfelt@gmail.com>
2012-09-19 22:22:12 -0500

    first commit

---
+++ src/com/example/digplay/MainMenu.java
@@ -0,0 +1,20 @@
+package com.example.digplay;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainMenu extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main_menu, menu);
+        return true;
+    }
+}
