b601e7886cb6186f06933827ad3b48ca195f6471
mirlin510 <michael.r.dotson@gmail.com>
2012-10-22 13:47:27 -0500

    first commit, just adding picture of field

--- src/com/example/digplay/EditorActivity.java
+++ src/com/example/digplay/EditorActivity.java
@@ -1,15 +1,26 @@
 package com.example.digplay;
 
+import com.businessclasses.Field;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class EditorActivity extends Activity {
 
+	private ImageView image;
+	private Field theField;
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.editor);
+	    
+	    image = (ImageView) findViewById(R.id.imageView1);
+	    theField = new Field();
+	    
 	}
+	
 
 }
