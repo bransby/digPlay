9693474d61c58f9b7c17617dfceb7bc56fadf3a1
krebsba3 <krebsba3.uwm.edu>
2012-10-24 19:19:24 -0500

    added some stuff to db

---
+++ src/com/database/DigPlayDBData.java
@@ -0,0 +1,52 @@
+package com.database;
+
+import java.util.ArrayList;
+import com.businessclasses.Field;
+
+public class DigPlayDBData {
+	
+	//private vars for the database
+	private long id;
+	private String playName;
+	private Field field;
+	private ArrayList<String> gamePlans;
+	private Byte[] image;
+	
+	//getters
+	public long getId(){
+		return id;
+	}
+	public String getPlayName(){
+		return playName;
+	}	
+	public Field getField(){
+		return this.field;
+	}
+	public ArrayList<String> getGamePlans(){
+		return this.gamePlans;
+	}
+	public Byte[] getImage(){
+		return this.image.getBlob();
+	}
+	
+	
+	//setters
+	public void setId(long _id){
+		this.id = _id;
+	}
+	public void setPlayName(String _playName){
+		this.playName = _playName;
+	}	
+	public void setField(Field _field){
+		this.field = _field;
+	}
+	public void setGamePlans(ArrayList<String> _gamePlans){
+		this.gamePlans = _gamePlans;
+	}	
+	public void setImage(byte[] _image){
+		this.image = _image;
+	}
+	public String toString(){
+		return playName;
+	}
+}
