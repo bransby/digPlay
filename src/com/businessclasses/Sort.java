2ce4e8868c81882df1fa77424ae297c560e48763
Zach <zacharyknanfelt@gmail.com>
2012-12-08 23:33:04 -0600

    fixing sorting, starting to implement email

--- src/com/businessclasses/Sort.java
+++ src/com/businessclasses/Sort.java
@@ -8,22 +8,27 @@ import android.content.ContextWrapper;
 public class Sort {
 	public PlayAdapter sortPlaysByRunPass(PlayAdapter adapter,String playType){
 		if(playType.equals("ALL PLAYS"))return adapter;
-		for(int i = 0;i < adapter.getCount();i++){
+		int totalCount = adapter.getCount();
+		for(int i = 0; i < totalCount; i++){
 			Field play = adapter.getItem(i);
-			if(!playType.equals(play.getPlayType())){
+			if(!playType.equalsIgnoreCase(play.getPlayType())){
 				adapter.remove(play);
+				i--; totalCount--;
 			}
 		}
 		return adapter;
 	}
 
-	public PlayAdapter sortPlaysByPlaybook(PlayAdapter adapter, String playbook, ArrayList<String> listOfGamePlans) {
+	public PlayAdapter sortPlaysByPlaybook(PlayAdapter adapter, String playbook, ArrayList<String> listOfPlaysInGameplan) {
 		if (playbook.equals("All Gameplans"))return adapter;
-		for(int i = 0; i < adapter.getCount(); i++)
+		int totalCount = adapter.getCount();
+		for(int i = 0; i < totalCount; i++)
 		{
 			Field play = adapter.getItem(i);
-			if(!listOfGamePlans.contains(play));
+			if(!listOfPlaysInGameplan.contains(play.getPlayName().toString())){
 				adapter.remove(play);
+				i--; totalCount--;
+			}
 		}
 		return adapter;
 	}
